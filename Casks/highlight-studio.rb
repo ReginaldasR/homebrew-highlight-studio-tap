1a2c0825c8c3e9be4642f4964b631dee7bad377b
Reginaldas Raila <ReginaldasR@users.noreply.github.com>
2026-04-14 00:12:17 +0300

    Update SHA256 checksum for highlight-studio

--- Casks/highlight-studio.rb
+++ Casks/highlight-studio.rb
@@ -1,6 +1,6 @@
   cask "highlight-studio" do
     version "0.1.0"
-    sha256 "d3c80f4e82e205be630be769a61306eaf2a59a0be21d47c7a535cb0c78abdb09"
+    sha256 "0c5608127e8977c8c0c413e188ef98acd2083cb149e51e82c1ef6fb33be2eda1"
 
     url "https://pub-6f1ad3727de34676a913c03751f097e4.r2.dev/releases/#{version}/Video-Highlight-Finder.app.zip"
     name "Highlight Studio"
