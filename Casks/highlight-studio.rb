990f52ae7a16a723a3bda12d70b1438793e7135f
Reginaldas Raila <ReginaldasR@users.noreply.github.com>
2026-04-14 09:19:18 +0300

    Update highlight-studio.rb

--- Casks/highlight-studio.rb
+++ Casks/highlight-studio.rb
@@ -1,6 +1,6 @@
   cask "highlight-studio" do
     version "0.1.0"
-    sha256 "a8dcedfc30e16c4864b350c04f471c7c62bd9c5ff8d30b849d63e894f31d1c2c"
+    sha256 "5f0a988e6982d40f0f89eb50254c46d31af7fae76719256537ea6e69cd1c3472"
 
     url "https://pub-6f1ad3727de34676a913c03751f097e4.r2.dev/releases/#{version}/Video-Highlight-Finder.app.zip"
     name "Highlight Studio"
